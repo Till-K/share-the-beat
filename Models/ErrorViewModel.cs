ae6a1f85b800b6687b0dd970b7796397c6a1aa3d
Till Kämmerer <till.kaemmerer@outlook.de>
2018-10-04 18:16:39 +0200

    Umstellung auf C#

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace share_the_beat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
